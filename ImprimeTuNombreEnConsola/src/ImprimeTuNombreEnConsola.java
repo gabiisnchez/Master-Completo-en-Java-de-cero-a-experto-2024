47d9689b657ca5064be22a6ac2c3d497d236a3cc
gabiisnchez <gabisahe@gmail.com>
2024-04-11 14:54:23 +0200

    Sección 1: Introducción

---
+++ ImprimeTuNombreEnConsola/src/ImprimeTuNombreEnConsola.java
@@ -0,0 +1,6 @@
+public class ImprimeTuNombreEnConsola {
+    public static void main(String[] args) {
+        String nombre = "Gabriel";
+        System.out.println("Hola, mi nombre es " + nombre);
+    }
+}
