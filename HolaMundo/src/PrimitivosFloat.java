c8acada0b9d9b26f302844a2fc7ed4170bd9f1a1
gabiisnchez <gabisahe@gmail.com>
2024-04-22 17:41:32 +0200

    Primitivos numéricos de punto flotante

---
+++ HolaMundo/src/PrimitivosFloat.java
@@ -0,0 +1,11 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+        float realFloat = 0.00000000015F; //1.5e-10F;
+        System.out.println("realFloat = " + realFloat);
+        
+        double realDouble = 1.5;
+        System.out.println("realDouble = " + realDouble);
+        
+    }
+}
