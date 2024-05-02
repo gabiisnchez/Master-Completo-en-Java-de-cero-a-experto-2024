74e5275250058f9b41d752eec40e3e5e76596477
gabiisnchez <gabisahe@gmail.com>
2024-05-02 17:00:21 +0200

    Métodos importantes del objeto String

---
+++ HolaMundo/src/EjemploStringMeteodos.java
@@ -0,0 +1,37 @@
+public class EjemploStringMeteodos {
+    public static void main(String[] args) {
+
+        String nombre = "Gabriel";
+
+        System.out.println("nombre.length() = " + nombre.length());
+        System.out.println("nombre.toUpperCase() = " + nombre.toUpperCase());
+        System.out.println("nombre.toLowerCase() = " + nombre.toLowerCase());
+        System.out.println("nombre.equals(\"Gabriel\") = " + nombre.equals("Gabriel"));
+        System.out.println("nombre.equals(\"gabriel\") = " + nombre.equals("gabriel"));
+        System.out.println("nombre.equalsIgnoreCase(\"gabriel\") = " + nombre.equalsIgnoreCase("gabriel"));
+        System.out.println("nombre.compareTo(\"Gabriel\") = " + nombre.compareTo("Gabriel"));
+        System.out.println("nombre.compareTo(\"Andres\") = " + nombre.compareTo("Andres"));
+        System.out.println("nombre.charAt(0) = " + nombre.charAt(0));
+        System.out.println("nombre.charAt(1) = " + nombre.charAt(1));
+        System.out.println("nombre.charAt(6) = " + nombre.charAt(6));
+        System.out.println("nombre.charAt(nombre.length()-1) = " + nombre.charAt(nombre.length()-1));
+
+        System.out.println("nombre.substring(1) = " + nombre.substring(1));
+        System.out.println("nombre.substring(1, 5) = " + nombre.substring(1, 5));
+        System.out.println("nombre.substring(4, 7) = " + nombre.substring(4, 7));
+        System.out.println("nombre.substring(nombre.length()-2) = " + nombre.substring(nombre.length()-2));
+
+        String trabalenguas = "trabalenguas";
+        System.out.println("trabalenguas.replace(\"a\", \".\") = " + trabalenguas.replace("a", "."));
+        System.out.println("trabalenguas = " + trabalenguas);
+        System.out.println("trabalenguas.indexOf('a') = " + trabalenguas.indexOf('a'));
+        System.out.println("trabalenguas.lastIndexOf('a') = " + trabalenguas.lastIndexOf('a'));
+        System.out.println("trabalenguas.indexOf('z') = " + trabalenguas.indexOf('z'));
+        System.out.println("trabalenguas.contains(\"traba\") = " + trabalenguas.contains("traba"));
+        System.out.println("trabalenguas.startsWith(\"traba\") = " + trabalenguas.startsWith("traba"));
+        System.out.println("trabalenguas.endsWith(\"lenguas\") = " + trabalenguas.endsWith("lenguas"));
+        System.out.println("  trabalenguas ");
+        System.out.println("  trabalenguas ".trim());
+
+    }
+}
